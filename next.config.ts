b7396c601533bd1adacc61cdaa9b422b489ef0d5
Z User <z@container>
2026-04-08 17:54:08 +0000

    d6ae6773-bf02-43e1-a815-d8f91361b3a8

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,7 @@ const nextConfig: NextConfig = {
     ignoreBuildErrors: true,
   },
   reactStrictMode: false,
+  serverExternalPackages: ["@libsql/client", "@prisma/adapter-libsql"],
 };
 
 export default nextConfig;
